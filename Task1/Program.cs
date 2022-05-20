0d110e1979e125c6b19d8e3a975326211839bbd3
DENIS BLINOV <blinovda1@mail.ru>
2022-05-20 07:34:59 +0300

    add decision

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,35 +1,64 @@
 ﻿// Принимает на вход координаты точки (X,Y), причём X не равно 0 и Y не равно нулю и
 // выдаёт номер четверти плоскости, в которой находится эта точка.
 
+// void Quarters(int x, int y)
+// {
+//     if (x > 0)
+//     {
+//         if (y > 0)
+//         {
+//             Console.WriteLine("I");
+//         }
+//         else if (y < 0)
+//         {
+//             Console.WriteLine("IV");
+//         }
+//     }
+
+//     else if (x < 0)
+//         if (y > 0)
+//         {
+//             Console.WriteLine("II");
+//         }
+//         else if (y < 0)
+//         {
+//             Console.WriteLine("III");
+//         }
+// }
+
+// Quarters(2, 2);
+// Quarters(-2, 2);
+// Quarters(-2, -2);
+// Quarters(2, -2);
+// Quarters(2, 0);
+// Quarters(0, 2);
+
 void Quarters(int x, int y)
 {
-    if (x > 0)
+    if(x==0||y==0)
     {
-        if (y > 0)
-        {
-            Console.WriteLine("I");
-        }
-        else if (y < 0)
-        {
-            Console.WriteLine("IV");
-        }
+        Console.WriteLine("=0");
     }
-
-    else if (x < 0)
-        if (y > 0)
-        {
-            Console.WriteLine("II");
-        }
-        else if (y < 0)
-        {
-            Console.WriteLine("III");
-        }
+    else if(x>0 & y>0)
+    {
+        Console.WriteLine("I");
+    }
+    else if(x<0 & y>0)
+    {
+        Console.WriteLine("II");
+    }
+    else if(x<0 & y<0)
+    {
+        Console.WriteLine("III");
+    }
+    else if(x>0 & y<0)
+    {
+        Console.WriteLine("IV");
+    }
 }
-
 Quarters(2, 2);
 Quarters(-2, 2);
 Quarters(-2, -2);
 Quarters(2, -2);
 Quarters(2, 0);
 Quarters(0, 2);
-
