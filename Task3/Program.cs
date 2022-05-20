f724cd3ef7887763066494c5c08d541853c24d15
DENIS BLINOV <blinovda1@mail.ru>
2022-05-20 16:34:26 +0300

    add condition

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+// A(3,6); B(2,1) -> 5,09
+
