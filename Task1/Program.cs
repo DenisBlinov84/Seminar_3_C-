86cbb1e296021cc876a05cac8e58126e9550e96a
DENIS BLINOV <blinovda1@mail.ru>
2022-05-20 07:21:19 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход координаты точки (X,Y), причём X не равно 0 и Y не равно нулю и
+// выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void Quarters(int x, int y)
+{
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            Console.WriteLine("I");
+        }
+        else if (y < 0)
+        {
+            Console.WriteLine("IV");
+        }
+    }
+
+    else if (x < 0)
+        if (y > 0)
+        {
+            Console.WriteLine("II");
+        }
+        else if (y < 0)
+        {
+            Console.WriteLine("III");
+        }
+}
+
+Quarters(2, 2);
+Quarters(-2, 2);
+Quarters(-2, -2);
+Quarters(2, -2);
+Quarters(2, 0);
+Quarters(0, 2);
+
